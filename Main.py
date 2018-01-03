0dfcdbec364b39349af17c06d255e1bab6925dfe
BobMak <30277772+BobMak@users.noreply.github.com>
2018-01-02 17:26:35 -0800

    Updated Main.py

--- Main.py
+++ Main.py
@@ -7,23 +7,6 @@ import pygame
 import pygame.gfxdraw as gfx
 from Menus import main_menu, player_set
 
-#   Effects and particles with gfx lib  -   done
-#   complex Colliding   -   done
-#   enemies!
-#   speed    -   damage(?)
-#   turrets 1   -   refract!!!
-#   graphs for ML!  -   done
-#   modify deacceleration with regards to speed    -   done
-#   menues:
-#       pause bug    -    done
-#       ship picking menue - done
-#   better asteroids - done
-#   better explosions mechanic - done
-#   better hp mechanic - done
-#   wave system - doing
-#   modify FX system - done
-#   make damage system - done
-#          remember to kill all temporary Objects! (one bug fixed)
 
 pygame.init()
 
